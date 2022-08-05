f6cdb9c0720543e169d5f750126d24e263c2608b
oabustosb <oabustosb@unal.edu.co>
2022-08-04 22:12:02 -0500

    AutoEncoder experimentation

---
+++ notebooks/satellite/satellite_ae.py
@@ -0,0 +1,53 @@
+current_path = ""
+import os
+
+try:
+    from initialization import initialization
+except:
+    from notebooks.initialization import initialization
+
+parent_path = initialization("qaddemadac", "/home/oabustosb/Desktop/")
+
+
+import qmc.tf.layers as layers
+import qmc.tf.models as models
+
+import tensorflow as tf
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+from experiments import experiments
+
+from mlflow_create_experiment import mlflow_create_experiment
+
+
+setting = {
+    "z_name_of_experiment": 'autoencoder_satellite',
+    "z_run_name": "autoencoder",
+    "z_dataset": "satellite",
+    "z_select_best_experiment": True,
+    "z_threshold": 0.0,
+    "z_base_lr": 1e-3,
+    "z_end_lr": 1e-7,
+    "z_power": 1,
+    "z_decay_steps": int(1000),
+    "z_autoencoder_epochs": int(320),
+    "z_autoencoder_batch_size": int(256),
+}
+
+prod_settings = {
+    #"z_alpha": [0, 0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99, 1], 
+    "z_sequential": [(64,32,16),(128,64,32,8),(128,32,2),(64,20,10,4)],
+    "z_layer": ["relu", "sigmoid", "tanh"],
+    "z_regularizer": ["l2", "l1", "l1_l2"]
+}
+
+params_int = ["z_power", "z_decay_steps", "z_autoencoder_epochs", "z_autoencoder_batch_size"]
+params_float = ["z_sequential", "z_threshold", "z_base_lr", "z_end_lr"]
+params_boolean = []
+
+
+mlflow = mlflow_create_experiment(setting["z_name_of_experiment"], server="local")
+
+experiments(setting, prod_settings, params_int, params_float, mlflow, params_boolean)
--- src/anomalydetection/experiment_autoencoder.py
+++ src/anomalydetection/experiment_autoencoder.py
@@ -10,21 +10,20 @@ from encoder_decoder_creator import encoder_decoder_creator
 
 import tensorflow as tf
 
-import qaddemadac
 import anomaly_detector
-import adaptive_rff
+
 
 np.random.seed(42)
 tf.random.set_seed(42)
 
 
-def experiment_qaddemadac(X_train, y_train, X_test, y_test, settings, mlflow, best=False):
+def experiment_ae(X_train, y_train, X_test, y_test, settings, mlflow, best=False):
 
     for i, setting in enumerate(settings):
-        
+
         with mlflow.start_run(run_name=setting["z_run_name"]):
 
-            
+
             polinomial_decay = tf.keras.optimizers.schedules.PolynomialDecay(setting["z_base_lr"], \
                 setting["z_decay_steps"], setting["z_end_lr"], power=setting["z_power"])
             optimizer = tf.keras.optimizers.Adam(learning_rate=polinomial_decay)  # optimizer
@@ -36,11 +35,11 @@ def experiment_qaddemadac(X_train, y_train, X_test, y_test, settings, mlflow, be
 
             X = np.array(X)
 
-            
-            encoder, decoder = encoder_decoder_creator(input_size=X.shape[1], input_enc=setting["z_adaptive_input_dimension"], \
-                        sequential=setting["z_sequential"], layer=setting["z_layer"], regularizer=setting["z_regularizer"])
 
-            autoencoder = anomaly_detector.AnomalyDetector(X.shape[1], setting["z_adaptive_input_dimension"], \
+            encoder, decoder = encoder_decoder_creator(input_size=X.shape[1], input_enc=setting["z_sequential"][-1], \
+                        sequential=setting["z_sequential"][:-1], layer=setting["z_layer"], regularizer=setting["z_regularizer"])
+
+            autoencoder = anomaly_detector.AnomalyDetector(X.shape[1], setting["z_sequential"][-1], \
                      layer = setting["z_layer"], \
                      regularizer = setting["z_regularizer"], encoder=encoder, decoder=decoder)
 
@@ -49,28 +48,30 @@ def experiment_qaddemadac(X_train, y_train, X_test, y_test, settings, mlflow, be
             history = autoencoder.fit(X, X, 
                       epochs=setting["z_autoencoder_epochs"], 
                       batch_size=setting["z_autoencoder_batch_size"],
-                      shuffle=True)
-            
+                      shuffle=True, verbose=0)
+
             encoded_data = autoencoder.encoder(X)
 
             reconstruction = autoencoder.decoder(encoded_data)
             reconstruction = tf.cast(reconstruction, tf.float64)
+            reconstruction_loss = [ (np.linalg.norm(reconstruction[i]-X[i]))**2 for i in range(X.shape[0]) ]
 
-            
-            setting["z_threshold"] = np.percentile(reconstruction, 1-setting["z_nu"])
+            nu = np.sum(y_test) / len(y_test)
+            setting["z_threshold"] = np.percentile(reconstruction_loss, 100*(1-nu))
 
-            reconstruction_error = autoencoder.predict((X_test, X_test))
+            reconstruction_test = autoencoder.predict((X_test, X_test))
+            reconstruction_error = [ (np.linalg.norm(reconstruction_test[i]-X_test[i]))**2 for i in range(X_test.shape[0]) ]
 
-
-            preds = (reconstruction_error> setting["z_threshold"]).astype(int)
+            #print(reconstruction_error)
+            preds = (reconstruction_error > setting["z_threshold"]).astype(int)
 
             metrics = calculate_metrics(y_test, preds, reconstruction_error, setting["z_run_name"])
 
             mlflow.log_params(setting)
             mlflow.log_metrics(metrics)
             [mlflow.log_metric("loss", metric, i) for metric, i in enumerate(history.history["loss"])]
-            [mlflow.log_metric("reconstruction_loss", metric, i) for metric, i in enumerate(history.history["reconstruction_loss"])]
-            [mlflow.log_metric("probs_loss", metric, i) for metric, i in enumerate(history.history["probs_loss"])]
+            # [mlflow.log_metric("reconstruction_loss", metric, i) for metric, i in enumerate(history.history["reconstruction_loss"])]
+            # [mlflow.log_metric("probs_loss", metric, i) for metric, i in enumerate(history.history["probs_loss"])]
 
             if best:
                 np.savetxt(('artifacts/'+setting["z_name_of_experiment"]+'-preds.csv'), preds, delimiter=',')
@@ -78,5 +79,5 @@ def experiment_qaddemadac(X_train, y_train, X_test, y_test, settings, mlflow, be
                 np.savetxt(('artifacts/'+setting["z_name_of_experiment"]+'-scores.csv'), y_test_pred, delimiter=',')
                 mlflow.log_artifact(('artifacts/'+setting["z_name_of_experiment"]+'-scores.csv'))
 
-            print(f"experiment_dmkde_sgd {i} metrics {metrics}")
-            print(f"experiment_dmkde_sgd {i} threshold {setting['z_threshold']}")
+            print(f"experiment_ae {i} metrics {metrics}")
+            print(f"experiment_ae {i} threshold {setting['z_threshold']}")
